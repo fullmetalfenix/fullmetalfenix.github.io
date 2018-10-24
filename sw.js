e8d35583d32e4e240bed237f2ca89a60e75dea74
fullmetalfenix <jpihlk@yahoo.com>
2018-10-24 19:54:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', function(event){
+self.addEventListener('install', function(event){ 
     event.waitUntil(
         caches.open('portfolio-stuff').then(function(cache){
             return cache.addAll(
@@ -30,15 +30,17 @@ self.addEventListener('install', function(event){
 })
 
 
-
-
-self.addEventListener('fetch', function(event){
-
-event.respondWith(
-caches.match(event.request).then(function(response){
-    if(response) return response;
-    return fetch(event.request)
-})
-);
-});
-
+   self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request).then(function(response){
+            if(response.status == 404){ 
+                return new Response("Not Found! <a href='index.html'>Go Back</a>", {
+                    headers: {'Content-Type': "text/html"}
+                });
+            }
+            return response; //if not status 404, return response              
+          });
+        })
+      );
+    });
